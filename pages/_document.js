519629b0418c08de14f439ffef2fb8b988ede5b3
Said Simokovic <said.simokovic@digitecgalaxus.ch>
2023-05-02 20:41:33 +0200

    Revert "Header created"
    This reverts commit 3c13a81dfd414c04126eabc01be70685d3a43a62.

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,11 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-import HeaderComponent from '../components/headercomponent'
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <body>
-        <HeaderComponent/>
-        <Main/>
+        <Main />
         <NextScript />
       </body>
     </Html>
