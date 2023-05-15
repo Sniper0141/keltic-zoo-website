1f7faba964090669a6a746e6a6fc4aa39468a0e8
Sniper0141 <116571645+Sniper0141@users.noreply.github.com>
2023-05-15 20:34:28 +0200

    Update Header
    has now fixed position

--- components/headercomponent.jsx
+++ components/headercomponent.jsx
@@ -2,21 +2,24 @@ import Link from 'next/link';
 
 export default function HeaderComponent(){
     return(
-        <header className="flex" style={{backgroundColor: '#90a955'}}>
-            <div className="w-1/2">
-                <Link href="/" className="flex w-fit ml-10 md:ml-20 ">
-                    <img src="./Keltic_Zoo_Logo.svg" alt='keltic zoo logo' className='h-24'></img>
-                    <p className="text-3xl px-6 mt-auto mb-auto font-serif">Keltic Zoo</p>
-                </Link>
+        <header>
+            <div className="flex fixed w-screen" style={{backgroundColor: '#90a955'}}>
+                <div className="w-1/2">
+                    <Link href="/" className="flex w-fit ml-10 md:ml-20 ">
+                        <img src="./Keltic_Zoo_Logo.svg" alt='keltic zoo logo' className='h-24'></img>
+                        <p className="text-3xl px-6 mt-auto mb-auto font-serif">Keltic Zoo</p>
+                    </Link>
+                </div>
+                <div className="w-1/2">
+                    <nav className="flex flex-col md:flex-row gap-10 mt-8 md:justify-center">
+                        <Link href="/" className="w-fit bg-black border-black rounded-lg border-2  font-bold text-white text-2xl pb-1 px-4 hover:transition-transform duration-500 ease-out md:hover:scale-105">Home</Link>
+                        <Link href="/" className="w-fit bg-black border-black rounded-lg border-2  font-bold text-white text-2xl pb-1 px-4 hover:transition-transform duration-500 ease-out md:hover:scale-105">Animals</Link>
+                        <Link href="/" className="w-fit border-black rounded-lg border-2 font-bold text-2xl pb-1 px-4 hover:transition-transform duration-500 ease-out md:hover:scale-105">Privacy</Link>
+                        <Link href="/" className="w-fit border-black rounded-lg border-2 font-bold text-2xl pb-1 px-4 hover:transition-transform duration-500 ease-out md:hover:scale-105">About</Link>
+                    </nav>
+                </div>
             </div>
-            <div className="w-1/2">
-                <nav className="flex flex-col md:flex-row gap-10 mt-8 md:justify-center">
-                    <Link href="/" className="w-fit bg-black border-black rounded-lg border-2  font-bold text-white text-2xl pb-1 px-4 hover:transition-transform duration-500 ease-out md:hover:scale-105">Home</Link>
-                    <Link href="/" className="w-fit bg-black border-black rounded-lg border-2  font-bold text-white text-2xl pb-1 px-4 hover:transition-transform duration-500 ease-out md:hover:scale-105">Animals</Link>
-                    <Link href="/" className="w-fit border-black rounded-lg border-2 font-bold text-2xl pb-1 px-4 hover:transition-transform duration-500 ease-out md:hover:scale-105">Privacy</Link>
-                    <Link href="/" className="w-fit border-black rounded-lg border-2 font-bold text-2xl pb-1 px-4 hover:transition-transform duration-500 ease-out md:hover:scale-105">About</Link>
-                </nav>
-            </div>
+            <div className='h-24'></div>
         </header>
     )
 }
